dfc448e71c35754db056238a0ceaf41be798ab17
JJFAO <jjfalonso@gmail.com>
2020-08-01 18:02:48 -0300

    update currency rates when press CurrenciesBottom button

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import CurrenciesContainer from './src/screens/currencies/content/CurrenciesCont
 import CurrenciesBottom from './src/screens/currencies/CurrenciesBottom'
 import FavoritesTop from './src/screens/favorites/FavoritesTop'
 import FavoritesContainer from './src/screens/favorites/content/FavoritesContainer'
-import currencies from './src/constants/currencies'
+import currencies, { initialRates } from './src/constants/currencies'
 
 import { darkTheme } from './src/constants/colors'
 import { lightTheme } from './src/constants/colors'
@@ -15,6 +15,7 @@ import { darkTheme as defaultTheme } from './src/constants/colors'
 const windowHeigh = Dimensions.get('screen').height
 
 export default function App() {
+  const [lastRates, setLastRates] = useState(initialRates)
   const [ mainVisible, setMainVisible ] = useState(true)
   const [ fromCurrency, setFromCurrency ] = useState('ars')
   const [ amount, setAmount ] = useState('')
@@ -26,9 +27,19 @@ export default function App() {
     appTheme.name === 'darkTheme' ? setAppTheme(lightTheme) : setAppTheme(darkTheme)
   }
   const styles = getStyle(appTheme)
-
+  const updateRates = () => {
+    fetch(`https://api.exchangerate.host/latest?base=${fromCurrency}`)
+    .then(res => res.json())
+    .then(responseJson => {
+      setLastRates(responseJson)
+      console.log("updateRates -> responseJson", responseJson)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
   const addFavoriteCurrency = newCurrency => {
-    setFavoriteCurrencies( prevState => [...prevState, newCurrency] )
+    setFavoriteCurrencies( prevState => [...prevState, newCurrency])
   }
 
   const updateCurrency = ( name, isFavorite ) => {
@@ -62,7 +73,12 @@ export default function App() {
                 changeScreen={setMainVisible}
                 allCurrencies={allCurrencies}
               />
-              <CurrenciesBottom appTheme={appTheme} updateTheme={updateTheme} />
+              <CurrenciesBottom
+                appTheme={appTheme}
+                updateTheme={updateTheme}
+                lastRates={lastRates}
+                updateRates={updateRates}
+              />
             </Fragment>
           )
           :
